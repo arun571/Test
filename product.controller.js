89507f8c73409c4e193e0b19e4ced38f699618c2
arun571 <68108376+arun571@users.noreply.github.com>
2020-09-03 11:43:17 +0530

    Add files via upload

---
+++ product.controller.js
@@ -0,0 +1,24 @@
+const Product = require('../models/product.model');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test controller!');
+};
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
